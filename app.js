1f76162b11ecd17b5c639f33b5c483f253f50c6e
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:41:48 -0300

    Variables, datos y operadores - ejercicio 12

---
+++ app.js
@@ -0,0 +1,8 @@
+//Porcentaje
+
+//Crear un programa que pida al usuario ingresar un número, y luego el porcentaje que se desea obtener del mismo. Devolver un mensaje muestre el porcentaje de dicho número.
+
+const numero = prompt ("Por favor ingresa un número");
+const porcentaje = prompt ("Por favor ingresa el porcentaje");
+alert("El porcentaje es: " + numero * porcentaje / 100 + "%");
+
