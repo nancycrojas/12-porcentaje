8f18ca951a93d4877bc015819ebb45e07d88179b
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 21:47:32 -0300

    Agregando variable y template strings

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 
 //Crear un programa que pida al usuario ingresar un número, y luego el porcentaje que se desea obtener del mismo. Devolver un mensaje muestre el porcentaje de dicho número.
 
-const numero = prompt ("Por favor ingresa un número");
-const porcentaje = prompt ("Por favor ingresa el porcentaje");
-alert("El porcentaje es: " + numero * porcentaje / 100 + "%");
+const numero = Number(prompt("Por favor ingresa un número"));
+const porcentaje = Number(prompt("Por favor ingresa el porcentaje"));
+//alert("El porcentaje es: " + numero * porcentaje / 100 + "%");
+
+const resultado = (numero * porcentaje) / 100;
+alert(`El ${porcentaje} % de ${numero} es: ${resultado}.`);
+
+
 
